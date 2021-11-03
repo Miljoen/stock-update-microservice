72cc2b02b2d5d956124131faa280de665e529a0c
Yoeri Moens <yoeri.moens@pixelindustries.com>
2021-11-03 15:54:25 +0100

    Server at single port

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ const { makeExecutableSchema } = require('@graphql-tools/schema')
 ;(async () => {
     const app = express()
     const httpServer = createServer(app)
-    const wsServer = createServer(app)
 
     const schema = makeExecutableSchema({ typeDefs, resolvers })
     const server = new ApolloServer({ schema })
@@ -24,16 +23,11 @@ const { makeExecutableSchema } = require('@graphql-tools/schema')
 
     SubscriptionServer.create(
         { schema, execute, subscribe },
-        { server: wsServer, path: server.graphqlPath },
+        { server: httpServer, path: server.graphqlPath },
     )
 
     httpServer.listen(process.env.HTTP_PORT, () => {
-        console.log(
-            `🚀 HTTP server ready at :${process.env.HTTP_PORT}${server.graphqlPath}`,
-        )
+        console.log(`🚀 HTTP server ready at :${process.env.HTTP_PORT}${server.graphqlPath}`)
+        console.log(`🚀 Websocket server ready at :${process.env.HTTP_PORT}${server.graphqlPath}`)
     })
-
-    wsServer.listen(process.env.WS_PORT, () => console.log(
-        `🚀 Websocket server ready at :${process.env.WS_PORT}${server.graphqlPath}`,
-    ))
 })()
